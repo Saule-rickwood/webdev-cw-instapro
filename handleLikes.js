a137ab29f297c0c24940fb0aedad822774e10d15
Saule <saulerickwood25@gmail.com>
2025-10-06 12:48:16 +0100

    fixed the likes with no signing in

--- handleLikes.js
+++ handleLikes.js
@@ -1,11 +1,12 @@
 import { dislikePost, likePost } from "./api.js";
-import { getToken, posts, renderApp, setPosts } from "./index.js";
+import { getToken, posts, renderApp, setPosts, user } from "./index.js";
 
 export function handleLikes() {
   const likeButtons = document.querySelectorAll(".like-button");
   console.log(likeButtons);
   for (const likeButton of likeButtons) {
     likeButton.addEventListener("click", () => {
+      if (!user) return alert("Авторизуйтесь, чтобы лайкать посты")
       const isLiked = likeButton.dataset.liked;
       if (isLiked === "true") {
         dislikePost(likeButton.dataset.postId, getToken()).then((data) => {
